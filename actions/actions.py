2824ce049b36b363882c18e4d1a1740f2194594b
Zunhokim <zxcrtf245@naver.com>
2023-11-30 19:35:46 +0900

    slot 에러 메시지 수정

--- actions/actions.py
+++ actions/actions.py
@@ -49,7 +49,7 @@
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from typing import Any, Dict, List, Text
-
+from rasa_sdk.events import SlotSet
 import mysql.connector
 
 class ActionGetCarList(Action):
